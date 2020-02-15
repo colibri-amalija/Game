75e9014b95f3e02b7a4a6d1211cdce2bc4a3d863
Amalia <amalija.smagina.colibri@gmail.com>
2020-02-15 12:28:02 +0200

    New update

--- main.php
+++ main.php
@@ -12,13 +12,18 @@ $columns = $app->add('Columns');
 $col_1 = $columns->addColumn(3);
 $col_2 = $columns->addColumn(10);
 $col_3 = $columns->addColumn(3);
-$clicker = $col_2->add(["Button",$_SESSION["i"],"green fluid big"]);
-$clicker->on('click',function ($m) {
-  $_SESSION["i"] = $_SESSION["i"] + 1;
-    return $m;
-  });
+
+$val=$col_2->add(['FormField/Line','1']);
+
+
+$clicker = $col_2->add(["Button","*click here*","green fluid big"]);
+$clicker->js('click',new\atk4\ui\jsReload($val,['val'=>$val->jsInput()->val(new\atk4\ui\jsExpression('parseInt([])+1', [$val->jsInput()->val()]),$val->jsInput()->focus())]));
 
   $save = $col_2->add(["Button","save","blue big"]);
 
   $exit = $app->add(['Button',"Exit","red"]);
   $exit->link(["exit"]);
+
+
+$x2 = $col_3->add(["Button","x2 = 10$","inverted pink"]);
+$pus = $col_3->add(["Button","JA NE LOX, +100.000, а у тебя- ČURBĀNS GALVĀ!","inverted purple"]);
